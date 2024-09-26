8fc355c45fd6de3c706cd029955818da957d5cca
gourabofficial <gangulygourab3@gmail.com>
2024-09-26 19:50:37 +0530

    updated

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+// tailwind.config.js
+module.exports = {
+  content: ["./**/*.{html,js}"], // make sure the content paths include your HTML files
+  theme: {
+    extend: {},
+  },
+  darkMode: 'class', // this enables dark mode using the class strategy
+  plugins: [],
+}
